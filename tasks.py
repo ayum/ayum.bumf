1cef2074dff811e9202bff25ba33863f386cef84
Aleksey Mikhaylov <almikhayl@gmail.com>
2021-03-19 13:50:09 +0000

    build(preview): avtosozdanie izobrajeniya pervoy stranicq

--- tasks.py
+++ tasks.py
@@ -1,25 +1,32 @@
 import os
 from invoke import task
-from pathlib import Path as path
+from pathlib import Path
 from weasyprint import HTML, CSS
 from weasyprint.fonts import FontConfiguration
 
-paper_path = 'sobraniya/article.txt'
-paper_html = 'build/sobraniya/sobraniya.html'
+path = 'sobraniya/article.txt'
+paper_html = Path('build/sobraniya/sobraniya.html').resolve()
+paper_pdf = Path(paper_html).with_suffix('.pdf')
+paper_txt = Path(paper_html).with_suffix('.txt')
+paper_png = Path(paper_html).with_suffix('.png')
 
 
 @task
-def gulp(ctx, paper_path=paper_path):
-    cmd = f'./node_modules/.bin/gulp default --path={paper_path}'
+def gulp(ctx, path=path):
+    cmd = f'./node_modules/.bin/gulp default --path={path}'
     print(f'invoke: {cmd}')
     ctx.run(cmd)
 
 
 @task
-def bumf(ctx, paper_html=paper_html):
-    paper_html = path(paper_html).resolve()
-    paper_pdf = path(paper_html).with_suffix('.pdf')
-    paper_txt = path(paper_html).with_suffix('.txt')
+def preview(ctx, paper_pdf=paper_pdf, paper_png=paper_png):
+    cmd = f'convert -background white -alpha remove -density 100 {paper_pdf}[0] {paper_png}'
+    print(f'invoke: {cmd}')
+    ctx.run(cmd)
+
+
+@task(post=[preview])
+def bumf(ctx, paper_html=paper_html, paper_pdf=paper_pdf, paper_txt=paper_txt):
     cmd = f'weasyprint -e=utf8 -a={paper_txt} -v {paper_html} {paper_pdf}'
     print(f'invoke: {cmd}')
     ctx.run(cmd)
